9e603366ecad9d537e2827810c4a3dc278fa4a26
Mahmoud Samy <mahmoud30816186@f-eng.tanta.edu.eg>
2022-09-07 23:31:59 +0200

    [ Challenges ] isAdmissibleOverpayment

---
+++ challenges/isAdmissibleOverpayment.js
@@ -0,0 +1,20 @@
+function solution(prices, notes, x) {
+	let sensitivity = 0;
+
+	for (let i in prices) {
+		let [changePercent, type] = notes[i].split(" ");
+		changePercent = parseFloat(changePercent);
+		if (isNaN(changePercent)) continue;
+
+		let storePrice =
+			type == "higher"
+				? parseFloat(prices[i]) / (1 + changePercent / 100)
+				: parseFloat(prices[i]) / (1 - changePercent / 100);
+
+		const change = parseFloat(prices[i]) - storePrice;
+		sensitivity += change;
+	}
+
+	if ((x == 0 && sensitivity > 0) || parseInt(sensitivity) > x) return false;
+	return true;
+}
