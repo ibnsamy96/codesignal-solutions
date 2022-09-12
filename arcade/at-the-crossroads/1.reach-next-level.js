b09a8b51613687086638abf4dca31c2dc87c0e04
Mahmoud Samy <mahmoud30816186@f-eng.tanta.edu.eg>
2022-09-12 17:47:52 +0200

    [ Arcade | At the Crossroads ] 1. Reach Next Level

---
+++ arcade/at-the-crossroads/1.reach-next-level.js
@@ -0,0 +1,5 @@
+// problem: https://app.codesignal.com/arcade/code-arcade/at-the-crossroads/m9wjpkCjgofg7gs8N
+
+function solution(experience, threshold, reward) {
+	return experience + reward >= threshold;
+}
